0355d94270a3022ae4eb311a734fcb8bf2cb3c11
Seeni <olabodeodebunmiseeni@gmail.com>
2024-09-15 14:06:36 +0100

    update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DynamicSettings from "./components/organisms/DynamicSettings/DynamicSetti
 import VoiceChatDisclosure from "./components/organisms/VoiceChatDisclosure/VoiceChatDisclosure";
 import { Toaster } from "sonner";
 import InteractiveCoinSwap from "./components/organisms/InteractiveCoinSwap/InteractiveCoinSwap";
+import Slo from "./assets/images/slo.png";
 
 const components = [<VoiceChatDisclosure />, <DynamicSettings />];
 
@@ -10,19 +11,61 @@ function App() {
   return (
     <>
       <main>
-        <div className="app h-screen w-screen flex flex-wrap gap-2 p-2 components-container">
+        <div className="header-container p-2 pb-0">
+          <header className=" text-xl font-medium heading rounded-md pr-4 pl-2">
+            <a
+              href="https://x.com/bodeslomo"
+              target="_blank"
+              rel="noreferrer"
+              className="slo"
+            >
+              <img className="slo-image" alt="A bitmoji of Slo" src={Slo} />
+              Slo
+            </a>
+
+            <ul className="links flex ml-auto">
+              <li className="">
+                <a
+                  href="https://read.cv/bodeslomo"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Resume
+                </a>
+              </li>
+              <li className="">
+                <a
+                  href="https://x.com/bodeslomo"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {" "}
+                  Twitter
+                </a>
+              </li>
+              <li className="">
+                <a
+                  href="https://github.com/seeniolabode"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+              </li>
+            </ul>
+          </header>
+        </div>
+        <div className="app gap-2 p-2 components-container">
           {components.map((comp, i) => {
             return (
               <div
-                className="aspect-square border flex items-center justify-center rounded-sm xl:basis-1/4 md:basis-1/3 flex-grow basis-full single-component"
+                className="aspect-square border flex items-center justify-center rounded-sm single-component"
                 key={i}
               >
                 {comp}
               </div>
             );
           })}
-          <div className="aspect-square  border flex items-center justify-center rounded-sm xl:basis-1/4 md:basis-1/3 flex-grow basis-full single-component"></div>
-          <div className="aspect-square  border flex items-center justify-center rounded-sm xl:basis-1/4 md:basis-1/3 flex-grow basis-full single-component"></div>
         </div>
       </main>
       <Toaster />
